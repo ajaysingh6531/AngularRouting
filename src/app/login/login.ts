86bc465560716664653bdf9666dd29c6a1b59840
Ajay Singh <ajaysingh@Ajays-MacBook-Air.local>
2026-07-27 10:30:19 +0530

    validators

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-login',
-  imports: [Header,ReactiveFormsModule],
+  imports: [Header,ReactiveFormsModule,NgIf],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -26,9 +27,14 @@ export class Login {
   //for form Grouping
 
   profileForm=new FormGroup({
-    name1:new FormControl(),
-    password1:new FormControl(),
-    email1:new FormControl()
+    //here fault value will be empyt as ''
+    //if you want some default value  then name1:new FormControl('Hello');
+    name1:new FormControl('',[Validators.required]),
+    password1:new FormControl('',{validators:[
+                                    Validators.required,
+                                    Validators.minLength(5)
+                                      ]}),
+    email1:new FormControl('',{validators:[Validators.required,Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]})
 
   })
   onSubmit(){
@@ -41,5 +47,15 @@ export class Login {
       password1:'321',
       email1:'peter@gmail.com'
     })
+  }
+
+  get n1(){
+    return this .profileForm.get('name1');
+  }
+  get e1(){
+    return this .profileForm.get('email1');
+  }
+  get p1(){
+    return this .profileForm.get('password1');
   }
 }
