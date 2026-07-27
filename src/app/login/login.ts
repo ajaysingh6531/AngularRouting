3cbf4f1a45709717b144a87b800ed5e8301569f9
Ajay Singh <ajaysingh@Ajays-MacBook-Air.local>
2026-07-27 09:28:40 +0530

    form Group

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   imports: [Header,ReactiveFormsModule],
@@ -21,5 +21,25 @@ export class Login {
   setValues(){
     this.name.setValue('peter');
     this.password.setValue('petterr');
+  }
+
+  //for form Grouping
+
+  profileForm=new FormGroup({
+    name1:new FormControl(),
+    password1:new FormControl(),
+    email1:new FormControl()
+
+  })
+  onSubmit(){
+    console.log(this.profileForm.value);
+    console.log(this.profileForm.value.email1);
+  }
+  setval(){
+    this.profileForm.setValue({
+      name1:'peter',
+      password1:'321',
+      email1:'peter@gmail.com'
+    })
   }
 }
