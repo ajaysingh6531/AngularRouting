e596830e7bfa3c70676e7b451289e6e96ccb68fe
Ajay Singh <ajaysingh@Ajays-MacBook-Air.local>
2026-07-27 09:11:56 +0530

    forms

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [Header],
+  imports: [Header,ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  name=new FormControl();
+  //name=new FormControl('deepu);  for default values
+  password=new FormControl();
+
+  displayValues(){
+    this.name=this.name.value;
+    this.password=this.password.value;
+  }
+ 
+  setValues(){
+    this.name.setValue('peter');
+    this.password.setValue('petterr');
+  }
+}
